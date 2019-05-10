6d8ba2c80b8a06d6ee09ff635748467e3b3fd372
dachuanli <V-dachuanli@creditease.cn>
2019-05-10 10:52:27 +0800

    [add]完善swift中的计算属性

--- 属性.playground/Contents.swift
+++ 属性.playground/Contents.swift
@@ -7,6 +7,7 @@ struct FixedLengthRange {
 
 var rangeOfThreeItems = FixedLengthRange(firstValue: 0, length: 3)
 rangeOfThreeItems.firstValue = 6
+ //rangeOfThreeItems.length = 10  此处不能赋值的原因是，因为是常量存储属性，后期不能改变
 
 
 //rangeOfThreeItems
@@ -15,7 +16,7 @@ rangeOfThreeItems.firstValue = 6
 let  rangeOfFourItems = FixedLengthRange(firstValue: 0, length: 4)
 //rangeOfFourItems.firstValue = 6
 
-//延时加载存储属性  相当于oc里面得了懒加载了 lazy关键字
+//延时加载存储属性  相当于oc里面得了懒加载了 lazy关键字   只有第一次被调用的时候，才会去初始化属性值 用lazy关键字
 
 class DataImporter {
     /*
@@ -38,9 +39,44 @@ manager.data.append("Some more data")
 
 
 //只读计算属性    只有getter 和没有 setter 方法 的属性，叫只读计算属性
+struct Point {
+    var x = 0.0 , y = 0.0
+}
 
+struct Size {
+    var width = 0.0, height = 0.0
+}
+
+//setter 方法有参数   getter 方法 return返回值  setter 方法  入参
+struct Rect {
+    var origin = Point()
+    var size = Size()
+    var center : Point {
+        get {
+           let centerX = origin.x + (size.width / 2)
+           let centerY = origin.y + (size.height / 2)
+           return Point(x: centerX, y: centerY)
+        }
+//        set(newCenter) {
+//            origin.x = newCenter.x - (size.width / 2)
+//            origin.y = newCenter.y - (size.height / 2)
+//        }
+        set {
+            origin.x = newValue.x - (size.width / 2)
+            origin.y = newValue.y - (size.height / 2)  //这个是简化版的setter 方法 不写入参，参数可以使用newValue
+        }
+    }
+}
+
+var squar = Rect(origin: Point(x: 0.0, y: 0.0), size:Size(width: 10.0, height: 10.0) )
+print("square.origin is now at (\(squar.center.x), \(squar.center.y))")    //计算属性，这么多年之后，今天用swift写，仍然感觉很惊艳
+let initialSquareCenter = squar.center
+squar.center = Point(x: 15.0, y: 15.0)
+print("square.origin is now at (\(squar.origin.x), \(squar.origin.y))")
 //属性观察器
-
+/**
+ *{}
+ */
 /*
  *
  *

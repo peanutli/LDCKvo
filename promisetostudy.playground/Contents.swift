2dabba5ba0938b3f434cd3349a0d2db772655f23
dachuanli <V-dachuanli@creditease.cn>
2019-05-14 14:00:38 +0800

    [add]:学习swift的开源库

---
+++ promisetostudy.playground/Contents.swift
@@ -0,0 +1,18 @@
+import Cocoa
+/***
+            ---->  Sources.swift
+PromiseKit
+            ---->  Sources.objc
+ 
+ swift:
+ 1.Promise.swift
+ 2.Guarantee.swift
+ 3.Thenable.swift
+ 4.Catchable.swift
+ 
+ objc: 可能涉及和swift进行混合编译，具体还没有仔细看。
+ 
+ 
+ 
+ 
+ **/
